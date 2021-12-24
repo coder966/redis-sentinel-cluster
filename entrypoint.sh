4edd5ac76e651ab32771969f8e81c973059a0211
coder966 <coder966@gmail.com>
2021-12-24 21:59:16 +0300

    add ability to configure redis as an LRU cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,15 @@
 # this will run a cluster of redis servers on ports 6381 and 6382
 # will also run three redis sentinel at ports 26381, 26382, and 26383
 
-tmux new -s redis-6381 -d "redis-server ./redis-config.conf --port 6381"
-tmux new -s redis-6382 -d "redis-server ./redis-config.conf --port 6382"
+
+CACHE_OPTIONS=""
+if [ "$AS_CACHE" == "yes" ]
+then
+    CACHE_OPTIONS="--appendonly no --save '' --maxmemory-policy allkeys-lru --maxmemory-samples 5"
+fi
+
+tmux new -s redis-6381 -d "redis-server ./redis-config.conf --port 6381 $CACHE_OPTIONS"
+tmux new -s redis-6382 -d "redis-server ./redis-config.conf --port 6382 $CACHE_OPTIONS"
 
 tmux new -s redis-sentinel-26381 -d "redis-server ./redis-sentinel-config.conf --sentinel --port 26381"
 tmux new -s redis-sentinel-26382 -d "redis-server ./redis-sentinel-config.conf --sentinel --port 26382"
